eb4d3a89676be412445e770835ab5d6f6de69623
notradek <radeknotpaul@gmail.com>
2023-08-17 19:17:26 +0200

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+x=int(input('Enter the range:'))
+for i in range(1,x-1,-1):
+    if i%3==0 and i%5==0:
+        print('fizzbuzz')
+    elif i%3==0:
+        print('fizz')
+    elif i%5==0:
+        print('buzz')
+    else:
+        print(i)
